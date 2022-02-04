3be81f83188501b11c7f9f3ccdac5de990c3cc2a
DynamaxCandy <ewanesun@gmail.com>
2022-02-04 12:58:10 +0900

    add comments

--- palindrome.py
+++ palindrome.py
@@ -4,6 +4,6 @@ class Solution:
             return False
         reversedNum, tmp = 0, x
         while tmp:
-            reversedNum = reversedNum*10 + tmp%10
-            tmp //= 10
+            reversedNum = reversedNum*10 + tmp%10 # Get individual digits from right and put into correct postion in reversed number
+            tmp //= 10 # Get the next last digit
         return x == tmp
